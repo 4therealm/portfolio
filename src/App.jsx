7b63275f9e7548d6f15054cdf76feae558bd3236
John Doe <johndoe@example.com>
2023-02-25 10:46:38 -0600

    skills skeleton coded

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import './App.css';
 import Navigation from './components/Navigation'
 import About from './components/About';
+
 function App() {
   return (
     <div className="App">
