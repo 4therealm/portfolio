d294afe81a404e0b747f1be745e93376f617c5aa
John Doe <johndoe@example.com>
2023-02-25 10:30:54 -0600

    redesigned about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { about } from '../portfolioData'
 
 export default function About() {
-
+  const {name, role, description, resume, social } = about
   return(
-    <section id="about">
-      <h2>About Me</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad repellendus culpa ex labore ducimus ratione voluptas earum inventore qui in deserunt eaque, saepe nobis natus non alias, sit nesciunt omnis.</p>
+    <section id="about" className="container">
+    <h2>About me</h2>
+    <h3>{name}</h3>
+    <p>{role}</p> 
+    <p>{description}</p>
+    <ul className="social row">
+      <li className="col-12"><a href={resume}>Resume</a></li>
+      <li className="col-6"><a href={social.linkedin}>LinkedIn</a></li>
+      <li className="col-6"><a href={social.github}>Github</a></li>
+    </ul>
     </section>
   )
 }
---
+++ src/portfolioData.js
@@ -0,0 +1,93 @@
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Git',
+  'MongoDB',
+  'Mongoose',
+  'Mysql',
+  'sequelize',
+  'Express',
+  'Nodejs',
+  'OOP',
+  'ORM',
+  'jQuery',
+  'Project Management',
+  'Agile method',
+  'Jest',
+  'Critical thinking',
+  'Googling'
+
+]
+
+
+const about = {
+
+  name: 'Maxwell Walters',
+  role: 'Full-stack Developer',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+}
+
+
+export {about, projects, skills}
