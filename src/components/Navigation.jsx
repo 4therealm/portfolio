29f8f5b779e9c1d2daebb296054251b5fb9295bc
Maxwell Walters <johndoe@example.com>
2023-03-03 14:33:13 -0600

    deployment

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,8 +8,7 @@ function Navigation() {
     <nav className="navbar">
   
       <ul>
-        <li><a href="#about-me">About Me</a></li>
-        <li><a href="#skills">Skills</a></li>
+        <li><a href="#about">About Me</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
