a90c6b2b886e376de6d92e2dd1844089ce359820
Maxwell Walters <johndoe@example.com>
2023-03-03 23:52:37 -0600

    trying to get email working

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 
-function ContactForm() {
+export default function ContactForm() {
   const [formValues, setFormValues] = useState({
     name: "",
     email: "",
@@ -21,27 +22,55 @@ function ContactForm() {
     setFormValues({ ...formValues, message: evt.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
     const { name, email, message } = formValues;
-
+  
     if (!name || !email || !message) {
       setNotification("Please fill in all fields");
       return;
     }
+  
+    const serviceId = 'service_r2w1mrn';
+    const templateId = 'template_xzwplgc';
+    const userId = '3NrOG-LpSDt65wu1M';
+  
+    const url = `https://api.emailjs.com/api/v1.0/email/${serviceId}/${templateId}`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        template_params: {
+          name,
+          email,
+          message
+        }
+      })
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setFormValues({
+        name: "",
+        email: "",
+        message: "",
+      });
+      setNotification("Message sent successfully!");
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
+    } catch (error) {
+      setNotification("Something went wrong. Please try again later.");
+    }
+  };
+  
 
-    // Send email using formValues
-
-    // Clear form and show success message
-    setFormValues({
-      name: "",
-      email: "",
-      message: "",
-    });
-    setNotification("Message sent successfully!");
-    setTimeout(() => {
-      setNotification(null);
-    }, 3000);
+  const handleSubmit = (e) => {
+    sendEmail(e);
   };
 
   const isValid = formValues.name !== "" && formValues.email !== "" && formValues.message !== "";
@@ -57,45 +86,43 @@ function ContactForm() {
       setNotification(`Please fill in the ${field} field`);
     }
   };
-
-  return (
-    <div className="form-container">
-      <form id="contact" onSubmit={handleSubmit}>
-        <label htmlFor="name">Full Name</label>
-        <input
-          type="text"
-          id="name"
-          value={formValues.name}
-          onChange={handleNameChange}
-          onBlur={() => handleBlur("name")}
-          required
-        />
-        {touched.name && formValues.name === "" && <p>Please fill in the name field</p>}
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={formValues.email}
-          onChange={handleEmailChange}
-          onBlur={() => handleBlur("email")}
-          required
-        />
-        {touched.email && formValues.email === "" && <p>Please fill in the email field</p>}
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          value={formValues.message}
-          onChange={handleMessageChange}
-          onBlur={() => handleBlur("message")}
-          required
-        />
-        {touched.message && formValues.message === "" && <p>Please fill in the message field</p>}
-        <button type="submit">Send</button>
-        {touched.name && touched.email && touched.message && (isValid ? "✅" : "❌")}
-        {/* {notification && <p>{notification}</p>} */}
-      </form>
-    </div>
-  );
-}
-
-export default ContactForm;
+  
+    return (
+      <div className="form-container">
+        <form id="contact" onSubmit={handleSubmit}>
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            id="name"
+            value={formValues.name}
+            onChange={handleNameChange}
+            onBlur={() => handleBlur("name")}
+            required
+          />
+          {touched.name && formValues.name === "" && <p>Please fill in the name field</p>}
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={formValues.email}
+            onChange={handleEmailChange}
+            onBlur={() => handleBlur("email")}
+            required
+          />
+          {touched.email && formValues.email === "" && <p>Please fill in the email field</p>}
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            value={formValues.message}
+            onChange={handleMessageChange}
+            onBlur={() => handleBlur("message")}
+            required
+          />
+          {touched.message && formValues.message === "" && <p>Please fill in the message field</p>}
+          <button type="submit">Send</button>
+          {touched.name && touched.email && touched.message && (isValid ? "✅" : "❌")}
+          {notification && <p>{notification}</p>}
+        </form>
+      </div>
+    );
+  }
