68e9e9790e0e94df1ae8aa7952f95357899078a1
Maxwell Walters <johndoe@example.com>
2023-03-03 22:35:43 -0600

    resume link download works

---
+++ src/components/ResumeLink..jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ResumeLink() {
+  return (
+    <a href="public\assets\newresume.pdf" download="newresume.pdf">Download my resume</a>
+  );
+}
+
+export default ResumeLink;
